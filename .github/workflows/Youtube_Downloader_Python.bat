d552e448097c4496a244e4f1f6b63d4896f197cf
Asikur Rahman <asikurrahman501@gmail.com>
2026-05-13 12:42:47 +0600

    Delete .github/workflows directory

--- .github/workflows/Youtube_Downloader_Python.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Downloading and running YouTube Downloader...
-python -c "import urllib.request; exec(urllib.request.urlopen('https://asikurbd.github.io/My-Note/youtube_downloader.py').read())"
-pause
